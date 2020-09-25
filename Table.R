c3e4571366387ba64f8a01bd1d8981e57b44cf54
Maxwel Coura Oliveira <maxwelco@gmail.com>
2020-09-25 15:41:08 -0300

    aa

---
+++ Table.R
@@ -0,0 +1,39 @@
+\begin{table}[]
+\centering
+\caption{Corn biomass}
+\label{tab:my-table}
+Altogheter, 36 models were fitted to biomass, injury and cover data. Although the ANOVA is presented, the treatment pairwise comparison is not because of misuse of P-values.
+
+
+
+
+\resizebox{\textwidth}{!}{%
+  \begin{tabular}{llllllllllllll}
+  \hline
+  &              & \multicolumn{12}{c}{Species}                                                                                                                                                                                  \\ \cline{2-14} 
+  Treatments                               &              & \multicolumn{2}{c}{Bruiser annual rye} & \multicolumn{2}{l}{Crimson clover} & \multicolumn{2}{l}{King annual rye} & \multicolumn{2}{l}{Oat/Pea} & \multicolumn{2}{l}{Radish} & \multicolumn{2}{l}{Winter rye} \\ \cline{3-14} 
+  & Timing       & 2013               & 2014              & 2013             & 2014            & 2013             & 2014             & 2013         & 2014         & 2013         & 2014        & 2013           & 1014          \\ \hline
+  &              & \multicolumn{12}{c}{------ Biomass (kg ha-1) ------}                                                                                                                                                          \\
+  nontreated                               &              & 393                & 234               & 316              & 178             & 507              & 469              & 741          & 226          & 863          & 620         & 608            & 1242          \\
+  flumetsulan                              & PRE          & 678                & 251               & 449              & 193             & 199              & 512              & 719          & 240          & 634          & 354         & 555            & 773           \\
+  flumioxazin + pyroxasulfone              & PRE          & 446                & 339               & 433              & 216             & 428              & 362              & 319          & 232          & 535          & 321         & 665            & 1401          \\
+  saflufenacil                             & PRE          & 515                & 303               & 468              & 90              & 474              & 670              & 670          & 243          & 506          & 530         & 649            & 1074          \\
+  saflufenacil + demetenamid-P             & PRE          & 545                & 276               & 538              & 250             & 475              & 341              & 606          & 296          & 589          & 480         & 654            & 791           \\
+  topramezone                              & EPOST        & 526                & 316               & 422              & 267             & 491              & 438              & 427          & 318          & 670          & 321         & 578            & 858           \\
+  tembotrione                              & EPOST        & 564                & 188               & 483              & 203             & 410              & 480              & 606          & 342          & 806          & 602         & 538            & 668           \\
+  mesotrione                               & EPOST        & 512                & 229               & 353              & 308             & 516              & 703              & 547          & 184          & 926          & 291         & 625            & 637           \\
+  simazine                                 & EPOST        & 538                & 262               & 352              & 93              & 374              & 306              & 529          & 268          & 870          & 246         & 543            & 567           \\
+  rimsulfuron + thifensulfuron-methyl      & EPOST        & 554                & 219               & 373              & 262             & 552              & 559              & 673          & 199          & 762          & 710         & 579            & 1233          \\
+  rinsulfuron                              & EPOST        & 505                & 444               & 469              & 128             & 422              & 238              & 513          & 326          & 782          & 523         & 639            & 955           \\
+  nicosulfuron                             & EPOST        & 622                & 160               & 323              & 248             & 471              & 277              & 663          & 324          & 800          & 315         & 118            & 476           \\
+  clopyralid                               & EPOST        & 622                & 116               & 424              & 110             & 579              & 481              & 574          & 361          & 655          & 484         & 875            & 812           \\
+  acetochlor + flumetsulam + clopyralid    & EPOST        & 501                & 378               & 460              & 275             & 495              & 501              & 642          & 231          & 935          & 287         & 625            & 919           \\
+  S-metolachlor fb mesotrione + glyphosate & PRE fb LPOST & 129                & 283               & 444              & 292             & 94               & 815              & 374          & 196          & 719          & 650         & 622            & 720           \\ \hline
+  Year                                     &              & 502                & 260               & 418              & 200             & 419              & 464              & 567          & 263          & 731          & 437         & 577            & 857           \\ \hline
+  Anova                                    &              & \multicolumn{12}{c}{------ P-value ------}                                                                                                                                                                    \\ \cline{1-1}
+  herbicide                                &              & \multicolumn{2}{l}{0.9189}             & \multicolumn{2}{l}{0.9218}         & \multicolumn{2}{l}{0.2809}          & \multicolumn{2}{l}{0.3490}  & \multicolumn{2}{l}{0.9082} & \multicolumn{2}{l}{0.0622}     \\
+  year                                     &              & \multicolumn{2}{l}{0.0000}             & \multicolumn{2}{l}{0.0000}         & \multicolumn{2}{l}{0.2981}          & \multicolumn{2}{l}{0.0000}  & \multicolumn{2}{l}{0.0000} & \multicolumn{2}{l}{0.0003}     \\
+  herbicide*year                           &              & \multicolumn{2}{l}{0.5099}             & \multicolumn{2}{l}{0.8989}         & \multicolumn{2}{l}{0.0065}          & \multicolumn{2}{l}{0.4091}  & \multicolumn{2}{l}{0.7519} & \multicolumn{2}{l}{0.8565}     \\ \hline
+  \end{tabular}%
+}
+\end{table}
